39976a69a226a769ae4ecfb8f98750c3c9576db1
Aszmel <aszmel@o2.pl>
2019-03-16 23:15:09 +0100

    after simple useState, used useEffect, t.b.c.

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const AppFunction = () => {
   const [count, setCount] = useState(0);
@@ -6,10 +6,20 @@ const AppFunction = () => {
 
   const handleCount = () => setCount(prevCount => prevCount + 1);
   const handleReset = () => setCount(0);
-  const changeColor = () => setIsOn(!isOn);
+  const changeColor = () => setIsOn(prevIsOn => !prevIsOn);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times!`;
+  });
 
   return (
-    <div>
+    <div
+      style={{
+        height: "100vh",
+        width: "auto",
+        background: "pink"
+      }}
+    >
       <h2>Toggle buttons</h2>
       <button onClick={handleCount}>This was clicked {count} times</button>
       <button onClick={handleReset}>Reset count button</button>
